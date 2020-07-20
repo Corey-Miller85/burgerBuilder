46c2ee9ebfec0cd0c85b68c131b47bae01e5e93a
Corey Miller <coreyMiller353@gmail.com>
2020-07-20 19:09:27 -0400

    create header toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./Toolbar.module.css";
+const Toolbar = (props) => {
+	return (
+		<header className={style.Toolbar}>
+			<div>MENU</div>
+			<div>LOGO</div>
+			<nav>...</nav>
+		</header>
+	);
+};
+
+export default Toolbar;
